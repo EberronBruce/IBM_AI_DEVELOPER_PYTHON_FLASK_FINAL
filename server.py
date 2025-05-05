2d0a9bc2e077e87efd46967381cd1e7567476b86
Bruce Burgess <bruce@redravencomputing.com>
2025-05-05 17:53:14 -0400

    Fixed based off pylint

--- server.py
+++ server.py
@@ -1,15 +1,21 @@
-from flask import Flask, request, render_template, jsonify
+"""Flask server to use for analyzing emotions by inputed text"""
+
+from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
+    """ Use to fext the html page for the index"""
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def detect_emotion():
-    text = request.args.get("textToAnalyze") 
+    """
+        Used to call the endpoint to anazlye text and return the result
+    """
+    text = request.args.get("textToAnalyze")
     result, status_code = emotion_detector(text)
 
     if status_code == 400:
@@ -29,4 +35,4 @@ def detect_emotion():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
-
+    
